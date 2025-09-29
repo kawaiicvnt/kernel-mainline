W-Floyd/linux

[DIRTY] khaje && tb128fu support
to be upstreamed and cleaned.

Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

drivers/gpu/drm/msm/msm_mdss.c
@@ -747,6 +747,7 @@ static const struct of_device_id mdss_dt_match[] = {
 	{ .compatible = "qcom,sm6115-mdss", .data = &sm6115_data },
 	{ .compatible = "qcom,sm6125-mdss", .data = &sm6125_data },
 	{ .compatible = "qcom,sm6150-mdss", .data = &sm6150_data },
+	{ .compatible = "qcom,sm6225-mdss", .data = &sm6115_data },
 	{ .compatible = "qcom,sm6350-mdss", .data = &sm6350_data },
 	{ .compatible = "qcom,sm6375-mdss", .data = &sm6350_data },
 	{ .compatible = "qcom,sm7150-mdss", .data = &sm7150_data },

kawaiicvnt/kernel-mainline

drivers/gpu/drm/msm/msm_mdss.c
@@ -567,6 +567,7 @@ static const struct of_device_id mdss_dt_match[] = {
 	{ .compatible = "qcom,sm6115-mdss", .data = &data_76k8 },
 	{ .compatible = "qcom,sm6125-mdss", .data = &data_76k8 },
 	{ .compatible = "qcom,sm6150-mdss", .data = &data_76k8 },
+	{ .compatible = "qcom,sm6225-mdss", .data = &data_76k8 },
 	{ .compatible = "qcom,sm6350-mdss", .data = &data_76k8 },
 	{ .compatible = "qcom,sm6375-mdss", .data = &data_76k8 },
 	{ .compatible = "qcom,sm7150-mdss", .data = &data_76k8 },