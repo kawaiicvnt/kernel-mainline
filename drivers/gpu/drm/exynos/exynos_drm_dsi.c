d6cbfb5e57b238ed4194e195df980ec08926dc3e
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-02-22 12:11:51 -0500

    Revert "initial attempt to fix drm panel"
    This reverts commit 52f7b413608db19505c4648a271748ec64d55653.

--- drivers/gpu/drm/exynos/exynos_drm_dsi.c
+++ drivers/gpu/drm/exynos/exynos_drm_dsi.c
@@ -159,11 +159,6 @@ static const struct samsung_dsim_plat_data exynos7870_dsi_pdata = {
 	.host_ops = &exynos_dsi_exynos_host_ops,
 };
 
-static const struct samsung_dsim_plat_data zuma_dsi_pdata = {
-	.hw_type = DSIM_TYPE_ZUMA,
-	.host_ops = &exynos_dsi_exynos_host_ops,
-};
-
 static const struct of_device_id exynos_dsi_of_match[] = {
 	{
 		.compatible = "samsung,exynos3250-mipi-dsi",
@@ -188,10 +183,6 @@ static const struct of_device_id exynos_dsi_of_match[] = {
 	{
 		.compatible = "samsung,exynos7870-mipi-dsi",
 		.data = &exynos7870_dsi_pdata,
-	},
-	{
-		.compatible = "samsung,exynos-dsim",
-		.data = &zuma_dsi_pdata,
 	},
 	{ /* sentinel. */ }
 };
