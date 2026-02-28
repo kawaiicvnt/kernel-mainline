3e360c2d6956236114655da8141f5059724fff21
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-02-27 22:30:55 -0500

    clk-exynos-clkout.c: Add gs101-pmu compatibility node

--- drivers/clk/samsung/clk-exynos-clkout.c
+++ drivers/clk/samsung/clk-exynos-clkout.c
@@ -76,6 +76,9 @@ static const struct of_device_id exynos_clkout_ids[] = {
 	}, {
 		.compatible = "samsung,exynos5433-pmu",
 		.data = &exynos_clkout_exynos5,
+	}, {
+		.compatible = "google,gs101-pmu",
+		.data = &exynos_clkout_exynos5,
 	}, { }
 };
 
